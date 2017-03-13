251803dce0df7647dd5abf858243aab207df586b
prasanna1211 <prasanna.krystal@gmail.com>
2017-03-13 11:19:42 +0530

    Added test features to test

---
+++ examples/app/index.js
@@ -0,0 +1,18 @@
+/**
+ * Index page of the app.
+ * Routes handles the app's routing.
+ */
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    its working
+  </div>
+);
+/*
+ * Renders the app with the target container
+ */
+render(
+  <App />,
+  document.getElementById('app'));
