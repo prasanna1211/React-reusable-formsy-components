7940dd0b9a0a63d414799fc8b440a6f2ec1d46ce
prasanna1211 <prasanna.krystal@gmail.com>
2017-03-13 10:29:49 +0530

    initial npm setting

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var numFormatter = require('../index');
+
+describe(' numFormatter ', function() {
+  it(' is true ', function() {
+    expect(true);
+  });
+})
