20a215dadebef790055554bda069f5207fef2254
prasanna1211 <prasanna.krystal@gmail.com>
2017-03-15 16:06:07 +0530

    Added selectbox

--- tests/InputSelect.js
+++ tests/InputSelect.js
@@ -26,9 +26,74 @@ describe('<InputSelect /> component shallow tests', () => {
   };
 
   it('renders everything correctly', () => {
-    const wrapper = shallow(<InputSelect {...baseProps}/>);
-    console.log(' wrapper ', wrapper.debug());
+    const wrapper = shallow(<InputSelect {...baseProps} />);
     expect(wrapper.find('select').exists()).to.equal(true);
     expect(wrapper.find('option')).to.have.length(2+1);
+  });
+
+  it('passes props to wrapper div correctly', () => {
+    const newProps = Object.assign(baseProps, {
+      wrapperClass: React.PropTypes.string,
+      wrapperStyle: React.PropTypes.string,
+    });
+    const wrapper = shallow(<InputSelect {...baseProps} />);
+
+    expect(true);
+  });
+
+  it('passes props to select tag correctly', () => {
+    const newProps = Object.assign(baseProps, {
+      onChangeInput: React.PropTypes.func,
+      name: React.PropTypes.string.isRequired,
+      required: React.PropTypes.bool,
+      selectClass: React.PropTypes.string,
+    });
+    const wrapper = shallow(<InputSelect {...baseProps} />);
+    expect(true);
+  });
+
+  it('passes props to option tag correctly', () => {
+    const newProps = Object.assign(baseProps, {
+      value: React.PropTypes.any,
+      optionClass: React.PropTypes.string,
+    });
+    const wrapper = shallow(<InputSelect {...baseProps} />);
+
+    expect(true);
+  });
+
+  it('passes props to error message div correctly', () => {
+    const newProps = Object.assign(baseProps, {
+      errorClass: React.PropTypes.string,
+      inline: React.PropTypes.bool,
+    });
+    const wrapper = shallow(<InputSelect {...baseProps} />);
+
+    expect(true);
+  });
+
+  it('displays the error message inline or block correctly', () => {
+    const newProps = Object.assign(baseProps, {
+      errorClass: React.PropTypes.string,
+      inline: React.PropTypes.bool,
+    });
+    const wrapper = shallow(<InputSelect {...baseProps} />);
+
+    expect(true);
+  });
+
+});
+
+describe('<InputSelect /> component full DOM tests', () => {
+  it('on recieving new props for value, setState is called ', () => {
+    expect(true);
+  });
+
+  it('on setting state for value, props.setValue() and props.onChange() is called', () => {
+    expect(true);
+  });
+
+  it('on selecting a new item ', () => {
+    expect(true);
   });
 });
